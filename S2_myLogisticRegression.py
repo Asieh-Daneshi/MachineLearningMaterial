f6c1bd240212a52f856a63e64bcb3fd8840e7a43
Asieh Daneshi <asieh.daneshi@gmail.com>
2024-08-02 11:51:28 +0200

    Logistic Regression Example

---
+++ S2_myLogisticRegression.py
@@ -0,0 +1,11 @@
+# *****************************************************************************
+# Logistic Regression
+# *****************************************************************************
+from sklearn.linear_model import LogisticRegression
+
+lg_model=LogisticRegression()        # here, we introduce nb_model, which is the GaussianNB
+lg_model.fit(Xtrain, Ytrain)
+Ypred = lg_model.predict(Xtest)
+
+from sklearn.metrics import classification_report
+print(classification_report(Ytest,Ypred))
