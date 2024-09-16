1f31c7165a46c29c0c7ea31a06d096fc3a83aa02
Asieh Daneshi <asieh.daneshi@gmail.com>
2024-09-16 10:42:44 +0200

    Commented round1

--- S3_myNeuralNetwork.py
+++ S3_myNeuralNetwork.py
@@ -9,43 +9,23 @@ def train_model(Xtrain, Ytrain, num_nodes, dropout_prob, lr, batch_size, epochs)
         tf.keras.layers.Dropout(dropout_prob),
         tf.keras.layers.Dense(1, activation='sigmoid')      # "output layer" is just one node and the activation function is sigoid
         ])
+    
     # in tensorflow, after building the network, we have to compile it!
-    nn_model.compile(optimizer=tf.keras.optimizers.Adam(lr), loss='binary_crossentropy',metrics=['accuracy'])     
+    nn_model.compile(optimizer=tf.keras.optimizers.Adam(lr), loss='binary_crossentropy',metrics=['accuracy'])   
     # default learning rate is 0.001, which we are also using
     # here, we use "Adam" optimizer. However, there are lots of optimizers.
     # for metrics, the NN by deafult computes the "loss". When we add "accuracy", later we can plot both of them to see how our network works.
+    
+    
     
     # here, we train our model. "history" is all the stuff that our network went through during training!
     history=nn_model.fit(
         Xtrain, Ytrain, epochs=epochs, batch_size=batch_size, validation_split=0.2, verbose=0
     )       # "validation_split" determines the fraction of the training data that will be used for validation
-                # when we put "verbose=0" it means that we don't want to see whole training process, we just want to see the output!
+            # when we put "verbose=0" it means that we don't want to see whole training process, we just want to see the output!
     return nn_model, history
 
-# These plot functions are available on internet, at tensorflow pages!
-# A good thing about tensorflow is that it keeps track of everything (history). It gives us the opportunity to go over it later and plot it!
-# def plot_loss(history):
-#     plt.plot(history.history['loss'], label='loss')
-#     plt.plot(history.history['val_loss'], label='val_loss')
-#     plt.xlabel('Epoch')
-#     plt.ylabel('Binary crossentropy')
-#     plt.legend()
-#     plt.grid(True)
-#     plt.show()
-    
-    
-# def plot_accuracy(history):
-#     plt.plot(history.history['accuracy'], label='accuracy')
-#     plt.plot(history.history['val_accuracy'], label='val_accuracy')
-#     plt.xlabel('Epoch')
-#     plt.ylabel('Accuracy')
-#     plt.legend()
-#     plt.grid(True)
-#     plt.show()
-# ***********************************************************************   
-# plots
-# plot_loss(history)
-# plot_accuracy(history)
+# *****************************************************************************
 
 def plot_history(history):
     fig, (ax1, ax2) = plt.subplots(1,2, figsize=(10,4))
@@ -72,19 +52,19 @@ def plot_history(history):
 least_val_loss=float('inf')
 least_loss_model=None
 epochs=100
-for num_nodes in [16,32,64]:
-    for dropout_prob in [0, 0.2]:
+for num_nodes in [16,32,32]:
+    for dropout_prob in [0, 0.1]:
         for lr in [0.1, 0.005, 0.001]:
-            for batch_size in [32, 64, 128]:
+            for batch_size in [20, 50, 100]:
                 print(f" {num_nodes} nodes, dropout {dropout_prob}, lr {lr}, batch size {batch_size}")
                 model, history = train_model(Xtrain, Ytrain, num_nodes, dropout_prob, lr, batch_size, epochs)
                 # plot_loss(history)
                 # plot_loss(accuracy)
                 plot_history(history)
-                val_losss=model.evaluate(Xvalid, Yvalid)
-                model_loss=val_losss[0]
-                if val_loss < least_val_loss:
-                    least_val_loss =val_loss
+                val_loss=model.evaluate(Xvalid, Yvalid)
+                model_loss=val_loss[0]
+                if model_loss < least_val_loss:
+                    least_val_loss =model_loss
                     least_loss_model=model
                 
                 
