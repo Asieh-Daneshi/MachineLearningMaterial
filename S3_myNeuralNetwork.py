99f01748865846e1a1f747212e2ff63488e6a36f
Asieh Daneshi <asieh.daneshi@gmail.com>
2024-09-16 11:20:41 +0200

    NN finalized

--- S3_myNeuralNetwork.py
+++ S3_myNeuralNetwork.py
@@ -26,13 +26,14 @@ def train_model(Xtrain, Ytrain, num_nodes, dropout_prob, lr, batch_size, epochs)
     return nn_model, history
 
 # *****************************************************************************
-
+# this function is available on internet for free
 def plot_history(history):
-    fig, (ax1, ax2) = plt.subplots(1,2, figsize=(10,4))
-    ax1.plot(history.history['loss'], label='loss')
+    fig, (ax1, ax2) = plt.subplots(1,2, figsize=(12,6))
+    # loss is the loss on the training data, and val_loss is the loss on the validation data
+    ax1.plot(history.history['loss'], label='loss')     # on the left side, we plot loss during training phase. Remember that loss is computed be default
     ax1.plot(history.history['val_loss'], label='val_loss')
     ax1.set_xlabel('Epoch')
-    ax1.set_ylabel('Binary crossentropy')
+    ax1.set_ylabel('Binary crossentropy')       # the loss function in line 14 was binary crossentropy
     ax1.legend()
     ax1.grid(True)
 
@@ -49,6 +50,7 @@ def plot_history(history):
 
 
 # ***********************************************************************
+# in this block, we model our network for different number of nodes, dropout probability, learning rate, and batch size
 least_val_loss=float('inf')
 least_loss_model=None
 epochs=100
@@ -56,12 +58,17 @@ for num_nodes in [16,32,32]:
     for dropout_prob in [0, 0.1]:
         for lr in [0.1, 0.005, 0.001]:
             for batch_size in [20, 50, 100]:
+                # For each set of parameters, we want to see number of nodes, droupout probability, learning rate, and batch size
                 print(f" {num_nodes} nodes, dropout {dropout_prob}, lr {lr}, batch size {batch_size}")
+                # When we put f  a string, it means that whatever comes in front of it in {} must be replaced by its value. 
+                # For example, if I write f"result", the output will be "result", but if I write f"{result}", the output will be numerical values of the result!
                 model, history = train_model(Xtrain, Ytrain, num_nodes, dropout_prob, lr, batch_size, epochs)
                 # plot_loss(history)
                 # plot_loss(accuracy)
                 plot_history(history)
+                # here, we do whatever we did for training set, now for validation set
                 val_loss=model.evaluate(Xvalid, Yvalid)
+                # in this part, I am just finding the least validation loss that my model can achieve
                 model_loss=val_loss[0]
                 if model_loss < least_val_loss:
                     least_val_loss =model_loss
@@ -69,6 +76,9 @@ for num_nodes in [16,32,32]:
                 
                 
 Ypred=least_loss_model.predict(Xtest)       # since we used sigmoid function in the output layer, "Ypred" values are between 0 and 1
-Ypred=(Ypred > 0.5).astype(int)         # here, we pick Ypred values larger than 0.5 and convert them into integers. Then, the output will be 0s and 1s.
+Ypred=(Ypred > 0.5).astype(int)         # here, I just binarize the output values. Meaning that I pick Ypred values larger than 0.5 and convert them into integers. Then, the output will be 0s and 1s.
 Ypred=Ypred.reshape(-1,)            # here we convert our Ypred into a 1-dimensional array
+
+# here we can see the classification report of our NN like other classification methods that we discussed, and compare its accuracy with previous models
+from sklearn.metrics import classification_report
 print(classification_report(Ytest, Ypred))
