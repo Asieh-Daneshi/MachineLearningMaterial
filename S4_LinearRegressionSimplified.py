77a842139c1eb58be70fe7231b92bc71ba597220
Asieh Daneshi <asieh.daneshi@gmail.com>
2024-09-30 14:06:27 +0200

    multiple linear regression and curve fitting

--- S4_LinearRegressionSimplified.py
+++ S4_LinearRegressionSimplified.py
@@ -3,6 +3,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt    
+import tensorflow as tf
 # =============================================================================
 # df = pd.read_csv("S1_Session1Data.data") 
 cols = ["transaction date", "house age", "distance to the nearest MRT station", "number of convenience stores", "latitude", "longitude", "house price of unit area"]        # name of the columns from the other file called "Session1Data.names"
@@ -52,9 +53,83 @@ def myXY(dataframe, yLabel, xLabel=None):
     
     return data, X, Y
 
-_,xTrain, yTrain=myXY(train, ["house price of unit area"],["latitude"])
+_,xTrainHousePrice, yTrainHousePrice=myXY(train, ["house price of unit area"],["latitude"])
+_,xValHousePrice, yValHousePrice=myXY(val, ["house price of unit area"],["latitude"])
+_,xTestHousePrice, yTestHousePrice=myXY(test, ["house price of unit area"],["latitude"])
+
+# our linear regressor
+from sklearn.linear_model import LinearRegression
+Reg=LinearRegression()
+Reg.fit(xTrainHousePrice,yTrainHousePrice)
+print(Reg.coef_,Reg.intercept_)
+# getting R-squared (closer to 1, better)
+Reg.score(xTestHousePrice, yTestHousePrice)
+
+# plot the results
+plt.scatter(xTrainHousePrice, yTrainHousePrice)
+x=tf.linspace(24.9, 25.05, 100)
+plt.plot(x, Reg.predict(np.array(x).reshape(-1,1)), label="output of our linear regression model",color="green")
 
 
+# *****************************************************************************
+# **********************    Multiple linear regression   **********************
+# *****************************************************************************
+_,xTrainHousePrice, yTrainHousePrice=myXY(train, ["house price of unit area"],["latitude", "house age", "longitude"])
+_,xValHousePrice, yValHousePrice=myXY(val, ["house price of unit area"],["latitude", "house age", "longitude"])
+_,xTestHousePrice, yTestHousePrice=myXY(test, ["house price of unit area"],["latitude", "house age", "longitude"])
+
+# our multiple linear regressor
+from sklearn.linear_model import LinearRegression
+Reg=LinearRegression()
+Reg.fit(xTrainHousePrice,yTrainHousePrice)
+print(Reg.coef_,Reg.intercept_)
+# getting R-squared (closer to 1, better)
+Reg.score(xTestHousePrice, yTestHousePrice)
+
+# *****************************************************************************
+# *********************   nonlinear regression      ***************************
+# *****************************************************************************
+# curve fitting
+from scipy.optimize import curve_fit
+flat_xTrainHousePrice = xTrainHousePrice.flatten()
+flat_yTrainHousePrice = yTrainHousePrice.flatten()
+flat_xValHousePrice = xValHousePrice.flatten()
+flat_yValHousePrice = yValHousePrice.flatten()
+flat_xTestHousePrice = xTestHousePrice.flatten()
+flat_yTestHousePrice = yTestHousePrice.flatten()
+def model_func(x, a, b, c):
+    return a * x**2 + b * x + c
+params, params_covariance = curve_fit(model_func, flat_xTrainHousePrice, flat_yTrainHousePrice)
+
+# Predict using the fitted parameters
+yPred = model_func(flat_xTrainHousePrice, *params)
+plt.scatter(flat_xTrainHousePrice, flat_yTrainHousePrice, color='blue')        
+plt.plot(flat_xTrainHousePrice, yPred, color='red')        
+plt.xlabel('X')
+plt.ylabel('y')
+plt.title('Non-linear Regression with curve_fit')
+plt.show()
+
+print("Fitted parameters:", params)
+# =============================================================================
+# simpler example 
+X = np.array([1, 2, 3, 4, 5])
+y = np.array([1, 4, 9, 16, 25])
+params, params_covariance = curve_fit(model_func, X, y)
+
+
+y_pred = model_func(X, *params)
+
+
+plt.scatter(X, y, color='blue') 
+plt.plot(X, y_pred, color='red')     
+plt.xlabel('X')
+plt.ylabel('y')
+plt.title('Non-linear Regression with curve_fit')
+plt.show()
+
+# Print the parameters of the fitted model
+print("Fitted parameters:", params)
 # the difference between deepcopy and copy ====================================
 x1=[1, 2, [3, 4], 5]
 x2=copy.deepcopy(x1)
